df3c774d5bcdbee5063a63fb1def29a9b8fc21f4
Michael McDonald <mcdonald.michael.mn@gmail.com>
2014-09-01 02:10:12 -0500

    Placeholder controller for members page

---
+++ app/scripts/controllers/members.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name knottypinesApp.controller:MembersCtrl
+ * @description
+ * # MembersCtrl
+ * Controller of the knottypinesApp
+ */
+angular.module('knottypinesApp')
+  .controller('MembersCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
