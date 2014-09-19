44e9d09157b32a1baba01a4e555800c29a277795
Michael McDonald <mcdonald.michael.mn@gmail.com>
2014-09-18 22:51:48 -0500

    Members page uses accordion

--- app/scripts/controllers/members.js
+++ app/scripts/controllers/members.js
@@ -7,11 +7,41 @@
  * # MembersCtrl
  * Controller of the knottypinesApp
  */
-angular.module('knottypinesApp')
-  .controller('MembersCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+ angular.module('knottypinesApp')
+ .controller('MembersCtrl', function ($scope) {
+
+	$scope.oneAtATime = false;
+
+	$scope.members = [
+	{
+		name: 'Trevor',
+		role: 'Lead Vocals, Guitar',
+		content: 'Trevor is the reason this band can do anything. He\'s pretty much a badass',
+		picture: 'http://greatcollegeadvice.com/wp-content/uploads/2008/07/MM-Square-face-for-thumbnail.jpg'
+	},
+	{
+		name: 'Michael',
+		role: 'Lead Guitar, Vocals',
+		content: 'Stuff',
+		picture: 'http://greatcollegeadvice.com/wp-content/uploads/2008/07/MM-Square-face-for-thumbnail.jpg'
+	},
+	{
+		name: 'Graham',
+		role: 'Percussion',
+		content: 'Stuff',
+		picture: 'http://greatcollegeadvice.com/wp-content/uploads/2008/07/MM-Square-face-for-thumbnail.jpg'
+	},
+	{
+		name: 'Ben',
+		content: 'Stuff',
+		picture: 'http://greatcollegeadvice.com/wp-content/uploads/2008/07/MM-Square-face-for-thumbnail.jpg'
+	}
+	];
+
+	$scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+	$scope.status = {
+		isFirstOpen: true,
+		isFirstDisabled: false
+	};
+ });
