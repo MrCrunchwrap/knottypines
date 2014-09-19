e66d43d72bfaf7acb8607b8b2cf3217fa9c127e3
Michael McDonald <mcdonald.michael.mn@gmail.com>
2014-09-18 22:53:36 -0500

    Basic template for music partial view

---
+++ app/scripts/controllers/music.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name knottypinesApp.controller:MusicCtrl
+ * @description
+ * # MusicCtrl
+ * Controller of the knottypinesApp
+ */
+angular.module('knottypinesApp')
+  .controller('MusicCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
